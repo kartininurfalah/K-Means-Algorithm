cc6a75034df4db0188fd0374735d55d4c4e5cb23
kartininurfalah <kartininurfalah@gmail.com>
2018-04-05 08:55:11 +0700

    modify k-means algorithm

--- k-means.py
+++ k-means.py
@@ -2,6 +2,7 @@ import nltk
 import numpy as np
 import pandas as pd
 import math
+from math import sqrt
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from pylab import genfromtxt
@@ -15,32 +16,59 @@ from random import randint
 dataTrain = np.genfromtxt("/home/helmisatria/FALAH/Machine Learning/K-means/TrainsetTugas2.txt")
 dataTest = np.genfromtxt("/home/helmisatria/FALAH/Machine Learning/K-means/TestsetTugas2.txt")
 
-dTrainX1 = dataTrain[:,0]
-dTrainX2 = dataTrain[:,1]
-dtTrain = [dTrainX1, dTrainX2]
+dTrainX = dataTrain[:,0]
+dTrainY = dataTrain[:,1]
+dtTrain = [dTrainX, dTrainY]
 
-dTestX1 = dataTest[:,0]
-dTestX2 = dataTest[:,1]
-dtTest = [dTestX1, dTestX2]
+dTestX = dataTest[:,0]
+dTestY = dataTest[:,1]
+dtTest = [dTestX, dTestY]
 # =============================================================================
 # # datatrain and dataTest scater
 # =============================================================================
-X = np.array(list(zip(dTrainX1, dTrainX2)))
+X = np.array(list(zip(dTrainX, dTrainY)))
+#
+#plt.scatter(dTestX, dTestY)
+#plt.show()
 
-plt.scatter(dTestX1, dTestX2)
-plt.show()
+k = 2
+#
+def initialCentroid(k):    
+    centroidTemp = []
+    for i in range(k):
+       indexCentroid= randint(1,688)
+       centroid = dataTrain[indexCentroid]
+       centroidTemp.append(centroid)
+    return centroidTemp
 
-k = 5
-indexCentroid= randint(1,688)
-centroid = X[indexCentroid]
-plt.scatter(dTrainX1, dTrainX2)
-plt.scatter(centroid[0], centroid[1], marker = '*', s = 200)
+tempC = initialCentroid(k)
+centroidX = [item[0] for item in tempC]
+centroidY = [item[1] for item in tempC]
+
+plt.scatter(dTrainX, dTrainY)
+plt.scatter(centroidX, centroidY, marker = '*', s = 200)
+print(tempC)
 # =============================================================================
 # Euclidian distance function
 # =============================================================================
-def euclidFunct(x, y, ax=1):
-    return np.linalg.norm(x - y, axis = ax)
+def euclidFunct(xa, xb, ya, yb):
+    return sqrt((xa-xb)**2 + (ya-yb)**2)
 
-def kMeansAlgo():
-    dtCluster = []
-    
+
+def kMeansAlgo(dTrain, centroid):
+    temp = [0,0]*k
+    while (temp != centroid):
+        dtCluster = []
+        temp = centroid
+        for i,point in enumerate(dtTrain):
+            tempDist =[]
+            for j,pCentroid in enumerate(centroid):
+                tempDist.append(euclidFunct(point[0], pCentroid[0], point[1], pCentroid[1]))
+            minDist = min(tempDist)
+            indexCentroid = tempDist.index(minDist)
+            dtCluster.append(indexCentroid)
+            newCentroid = 0
+    return dtCluster
+
+clust = kMeansAlgo(dtTrain, tempC)
+print(clust)
