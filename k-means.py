6d565ae4f9a2c6e7f9dc8fdecf48d1db7302df98
kartininurfalah <kartininurfalah@gmail.com>
2018-04-08 21:43:08 +0700

    modify kmeans n SSE

--- k-means.py
+++ k-means.py
@@ -31,7 +31,7 @@ X = np.array(list(zip(dTrainX, dTrainY)))
 #plt.scatter(dTestX, dTestY)
 #plt.show()
 
-k = 2
+k = 5
 #
 def initialCentroid(k):    
     centroidTemp = []
@@ -41,12 +41,15 @@ def initialCentroid(k):
        centroidTemp.append(centroid)
     return centroidTemp
 
+def newList(w,h):
+    return [[0 for x in range(w)] for y in range(h)]
+
 tempC = initialCentroid(k)
 centroidX = [item[0] for item in tempC]
 centroidY = [item[1] for item in tempC]
 
-plt.scatter(dTrainX, dTrainY)
-plt.scatter(centroidX, centroidY, marker = '*', s = 200)
+#plt.scatter(dTrainX, dTrainY)
+#plt.scatter(centroidX, centroidY, marker = '*', s = 200)
 print(tempC)
 # =============================================================================
 # Euclidian distance function
@@ -54,21 +57,75 @@ print(tempC)
 def euclidFunct(xa, xb, ya, yb):
     return sqrt((xa-xb)**2 + (ya-yb)**2)
 
+#def kMeansAlgo(dTrain, centroid):
+#    temp = [0,0]*k
+#    while (temp != centroid):
+#        dtCluster = []
+##        temp = centroid
+#        for i,point in enumerate(dtTrain):
+#            tempDist = []
+#            for j,pCentroid in enumerate(centroid):
+#                tempDist.append(euclidFunct(point[0], pCentroid[0], point[1], pCentroid[1]))
+#            minDist = min(tempDist)
+#            indexCentroid = tempDist.index(minDist)
+#            dtCluster.append(indexCentroid)
+#            print(tempDist) 
+#    return dtCluster
+newCluster = [[0,0]] * k
+loop = 0
+while(np.any(np.not_equal(tempC, newCluster)) and (loop!=70)):
+    dtCluster = [] 
+    if(newCluster[0] != [0,0]):
+        tempC = newCluster                      
+    for i, point in enumerate(dataTrain):
+        tempDist = []
+        for j,pCentroid in enumerate(tempC):
+            tempDist.append(euclidFunct(point[0], pCentroid[0], point[1], pCentroid[1]))
+        minDist = min(tempDist)
+        indexCentroid = tempDist.index(minDist)
+        dtCluster.append(indexCentroid)
+    #    print(dtCluster)
+        
+    #clust = kMeansAlgo(dtTrain, tempC)
+    #print(clust)
+    
+    mergeDataClust = np.column_stack((dataTrain, dtCluster))
+    separatedCluster = newList(0, k)
+    
+    for p,xPoint in enumerate(mergeDataClust):
+        separatedCluster[int(xPoint[2])].append(xPoint)
+    
+    
+    for x,pClust in enumerate(separatedCluster):
+        dataX = []
+        dataY = []
+        for i,valClust in enumerate(pClust):
+            dataX.append(valClust[0])    
+            dataY.append(valClust[1])
+        newCluster[x] = [np.mean(dataX), np.mean(dataY)]
+    loop = loop+1
+    
+print(newCluster)
 
-def kMeansAlgo(dTrain, centroid):
-    temp = [0,0]*k
-    while (temp != centroid):
-        dtCluster = []
-        temp = centroid
-        for i,point in enumerate(dtTrain):
-            tempDist =[]
-            for j,pCentroid in enumerate(centroid):
-                tempDist.append(euclidFunct(point[0], pCentroid[0], point[1], pCentroid[1]))
-            minDist = min(tempDist)
-            indexCentroid = tempDist.index(minDist)
-            dtCluster.append(indexCentroid)
-            newCentroid = 0
-    return dtCluster
+clusterX = [item[0] for item in newCluster]
+clusterY = [item[1] for item in newCluster]
 
-clust = kMeansAlgo(dtTrain, tempC)
-print(clust)
+plt.scatter(dTrainX, dTrainY, c=dtCluster)
+plt.scatter(clusterX, clusterY, marker = '*', s = 333)
+plt.show()
+
+def sse():
+    sum = 0
+    for i,valCluster in enumerate(separatedCluster):
+        for j, pClust in enumerate(valCluster):
+            sum += euclidFunct(tempC[i][0], pClust[0], tempC[i][1], pClust[1])
+    return sum
+
+
+print('SSE : ',sse())
+
+sseCluster = [k, tempC, sse()]
+
+sseCluster = pd.DataFrame([sseCluster])
+
+    
